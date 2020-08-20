4af1d0adcbf3c0ccb99743803d6d9f661faae7f2
Joseandres Hinojoza <57422356+Ualabi@users.noreply.github.com>
2020-08-20 18:33:32 -0500

    Update A.py

--- Qualification Round/A/A.py
+++ Qualification Round/A/A.py
@@ -1,3 +1,5 @@
+# Link: https://www.facebook.com/codingcompetitions/hacker-cup/2020/qualification-round/problems/A
+
 import os
 mydir = os.path.dirname(os.path.abspath(__file__))
 mychar = '\\' if '\\' in mydir else '/'
